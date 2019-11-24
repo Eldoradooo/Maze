b4962c682bebd245719f93a85cc566660056493d
Krzysztof Mozdzynski <44206888+Eldoradooo@users.noreply.github.com>
2019-11-24 22:03:46 +0100

    still not working

--- src/main/java/maze/Maze.java
+++ src/main/java/maze/Maze.java
@@ -6,13 +6,14 @@ import java.io.FileNotFoundException;
 public class Maze {
     public static void main(String[] args) throws FileNotFoundException{
     MazeLoader maze1 = new MazeLoader();
+    MazeCreator c = new MazeCreator();
     File file = new File("maze.txt");
     int heigth = maze1.getHeigth(file);
     int width = maze1.getWidth(file);
     System.out.print("Heigth: " + heigth + " ");
     System.out.print("Width: " + width + "\n");
     String[][] result;
-    result = maze1.loadMaze(file);
+    result = c.createMaze(11, 11);
     for(int i = 0;i < heigth; i++) {
             for(int j = 0;j < width; j++) {
                 System.out.print(result[j][i]);
--- src/main/java/maze/MazeCreator.java
+++ src/main/java/maze/MazeCreator.java
@@ -6,6 +6,7 @@ public class MazeCreator {
 
     public String[][] createMaze(int width, int heigth) {
         String[][] maze = new String[width][heigth];
+        int dots = 0, paramSX = 0, paramSY = 0, paramEX = 0, paramEY = 0;
         for (int i = 0; i < heigth; i++) {
             for (int j = 0; j < width; j++) {
                 if (i == 0 || i == heigth - 1 || j == 0 || j == width - 1) {
@@ -15,18 +16,64 @@ public class MazeCreator {
                 }
             }
         }
-        // ściany są PARZYSTE
+        for (int i = 2; i < heigth; i = i + 2) {
+            for (int j = 2; j < width; j = j + 2) {
+                if ("0".equals(maze[j][i]) == true) {
+                    dots++;
+                }
+            }
+        }
+        while (dots != 0) {
+            for (int i = 2; i < heigth; i = i + 2) {
+                for (int j = 2; j < width; j = j + 2) {
+                    if ("0".equals(maze[j][i]) == true) {
+                        boolean rightNeighbor = true, downNeighbor = true;
+                        paramSX = j - 2;
+                        paramSY = i - 2;
+                        paramEX = j + 2;
+                        paramEY = i + 2;
+                        while (rightNeighbor != false && downNeighbor != false) {
+                            if ("0".equals(maze[paramEX - 2][paramEY]) == true && "0".equals(maze[paramEX][paramEY - 2]) == true) {
+                                paramEX = paramEX + 2;
+                                paramEY = paramEY + 2;
+                            } else if ("0".equals(maze[paramEX - 2][paramEY]) == true) {
+                                paramEY = paramEY + 2;
+                            } else if ("0".equals(maze[paramEX][paramEY - 2]) == true) {
+                                paramEX = paramEX + 2;
+                            } else {
+                                rightNeighbor = false;
+                                downNeighbor = false;
+                            }
+                        }
+                    }
+                }
+            }
+            maze = divideMaze(maze, paramSX, paramSY, paramEX, paramEY);
+            dots = 1;
+            for (int i = 2; i < heigth; i = i + 2) {
+                for (int j = 2; j < width; j = j + 2) {
+                    if ("0".equals(maze[j][i]) == true) {
+                        dots++;
+                    }
+                }
+            }
+            dots = dots - 1;
+        }
+        return maze;
 
     }
 
     public String[][] divideMaze(String[][] notDividedMaze, int sx, int sy, int ex, int ey) {
+        if ((ex - sx) * (ey - sy) == 4) {
+            return notDividedMaze;
+        }
         Random rand = new Random();
         int ver, x = sx, y = sy, w = 0, k = 0, area1 = 0, area2 = 0, wall = 0;
         if ((ex - sx) == (ey - sy)) {
-            ver = rand.nextInt(1);
+            ver = rand.nextInt(2);
             if (ver == 0) {
                 w = (ey - sy) / 2 - 2;
-                k = rand.nextInt(w) + 1;
+                k = rand.nextInt(w+1) + 1;
                 wall = k * 2;
                 for (int i = sx; i < ex; i++) {
                     notDividedMaze[i][wall] = "+";
@@ -34,7 +81,7 @@ public class MazeCreator {
                 area1 = (ex - sx - 2) * (k * 2 - sy - 2);
                 area2 = (ex - sx - 2) * (ey - k * 2 - 2);
                 w = (ex - sx) / 2 - 1;
-                k = rand.nextInt(w) + 1;
+                k = rand.nextInt(w+1) + 1;
                 notDividedMaze[k * 2 - 1][wall] = "0";
                 if (area1 > area2) {
                     divideMaze(notDividedMaze, sx, sy, ex, wall);
@@ -43,7 +90,7 @@ public class MazeCreator {
                 }
             } else if (ver == 1) {
                 w = (ex - sx) / 2 - 2;
-                k = rand.nextInt(w) + 1;
+                k = rand.nextInt(w+1) + 1;
                 wall = k * 2;
                 for (int i = sy; i < ey; i++) {
                     notDividedMaze[wall][i] = "+";
@@ -51,7 +98,7 @@ public class MazeCreator {
                 area1 = (k * 2 - sx - 2) * (ey - sy - 2);
                 area2 = (ex - k * 2 - 2) * (ey - sy - 2);
                 w = (ey - sy) / 2 - 1;
-                k = rand.nextInt(w) + 1;
+                k = rand.nextInt(w+1) + 1;
                 notDividedMaze[wall][k * 2 - 1] = "0";
                 if (area1 > area2) {
                     divideMaze(notDividedMaze, sx, sy, wall, ey);
@@ -61,7 +108,7 @@ public class MazeCreator {
             }
         } else if ((ex - sx) > (ey - sy)) {
             w = (ex - sx) / 2 - 2;
-            k = rand.nextInt(w) + 1;
+            k = rand.nextInt(w+1) + 1;
             wall = k * 2;
             for (int i = sy; i < ey; i++) {
                 notDividedMaze[wall][i] = "+";
@@ -69,7 +116,7 @@ public class MazeCreator {
             area1 = (k * 2 - sx - 2) * (ey - sy - 2);
             area2 = (ex - k * 2 - 2) * (ey - sy - 2);
             w = (ey - sy) / 2 - 1;
-            k = rand.nextInt(w) + 1;
+            k = rand.nextInt(w+1) + 1;
             notDividedMaze[wall][k * 2 - 1] = "0";
             if (area1 > area2) {
                 divideMaze(notDividedMaze, sx, sy, wall, ey);
@@ -79,7 +126,7 @@ public class MazeCreator {
 
         } else if ((ex - sx) < (ey - sy)) {
             w = (ey - sy) / 2 - 2;
-            k = rand.nextInt(w) + 1;
+            k = rand.nextInt(w+1) + 1;
             wall = k * 2;
             for (int i = sx; i < ex; i++) {
                 notDividedMaze[i][wall] = "+";
@@ -87,7 +134,7 @@ public class MazeCreator {
             area1 = (ex - sx - 2) * (k * 2 - sy - 2);
             area2 = (ex - sx - 2) * (ey - k * 2 - 2);
             w = (ex - sx) / 2 - 1;
-            k = rand.nextInt(w) + 1;
+            k = rand.nextInt(w+1) + 1;
             notDividedMaze[k * 2 - 1][wall] = "0";
             if (area1 > area2) {
                 divideMaze(notDividedMaze, sx, sy, ex, wall);
@@ -95,6 +142,7 @@ public class MazeCreator {
                 divideMaze(notDividedMaze, sx, wall, ex, ey);
             }
         }
+        return notDividedMaze;
     }
 
 }
