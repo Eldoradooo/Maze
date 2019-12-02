6511258cb0a18643529e6d71b947704a5b3cc69d
Krzysztof Mozdzynski <44206888+Eldoradooo@users.noreply.github.com>
2019-12-02 12:44:42 +0100

    All working + showed

--- src/main/java/maze/Maze.java
+++ src/main/java/maze/Maze.java
@@ -5,36 +5,54 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Maze {
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-    int heigth, width;
-    MazeLoader maze1 = new MazeLoader();
-    MazeCreator c = new MazeCreator();
-    MazeSaver save = new MazeSaver();
-    BFSAlgorithm bfs = new BFSAlgorithm();
-    TremauxAlgorithm tre = new TremauxAlgorithm();
-    File file = new File("maze.txt");
-   /* boolean exists = file.exists();
-    if(exists) {
-    heigth = maze1.getHeigth(file);
-    width = maze1.getWidth(file);
-    }
-    else { */
-    heigth = 11;
-    width = (int) 11;
-    // }
-    System.out.print("Heigth: " + heigth + " ");
-    System.out.print("Width: " + width + "\n");
-    String[][] result;
-    result = maze1.loadMaze(file);
-    //result = c.createMaze(11, 11);
-    result = tre.solveMazeWithTremaux(result, width, heigth);
-    for(int i = 0;i < 11; i++) {
-            for(int j = 0;j < 11; j++) {
-                System.out.print(result[j][i]);
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        int heigth, width;
+        heigth = 5;  //wysokość labiryntu
+        width = 5;  //szerokość labiryntu
+        heigth = heigth * 2 + 1;
+        width = width * 2 + 1;
+        MazeLoader maze1 = new MazeLoader();
+        MazeCreator c = new MazeCreator();
+        MazeSaver save = new MazeSaver();
+        BFSAlgorithm bfs = new BFSAlgorithm();
+        TremauxAlgorithm tre = new TremauxAlgorithm();
+        File file = new File("maze.txt");
+        String[][] result1, result2;
+        result1 = maze1.loadMaze(file);
+        System.out.print("Wczytany labirynt: \n");
+        for (int i = 0; i < heigth; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(result1[j][i]);
             }
             System.out.print("\n");
         }
-   // save.saveMaze(result);
-    
-  }
+        result2 = c.createMaze(width, heigth);
+        System.out.print("Wygenerowany labirynt: \n");
+        for (int i = 0; i < heigth; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(result2[j][i]);
+            }
+            System.out.print("\n");
+        }
+        save.saveMaze(result1);
+        System.out.print("Wygenerowany labirynt został zapisany w pliku 'generatedmaze.txt' \n");
+        result1 = tre.solveMazeWithTremaux(result1, width, heigth);
+        System.out.print("Znaleziona ścieżka za pomocą algorytmu Tremauxa: \n");
+        for (int i = 0; i < heigth; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(result1[j][i]);
+            }
+            System.out.print("\n");
+        }
+        result2 = bfs.solveMazeWithBFS(result2, width, heigth);
+        System.out.print("Znaleziona ścieżka za pomocą algorytmu BFS: \n");
+        for (int i = 0; i < heigth; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(result2[j][i]);
+            }
+            System.out.print("\n");
+        }
+
+    }
 }
--- src/main/java/maze/MazeSaver.java
+++ src/main/java/maze/MazeSaver.java
@@ -8,7 +8,7 @@ public class MazeSaver {
     public void saveMaze(String[][] resolvedMaze) throws IOException  {
         MazeLoader m = new MazeLoader();
         
-        BufferedWriter writer = new BufferedWriter(new FileWriter("solvedmaze.txt", true));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("generatedmaze.txt", true));
         for(int i = 0;i < resolvedMaze[0].length; i++) {
             for(int j = 0;j < resolvedMaze[0].length; j++) {
                 writer.write(resolvedMaze[j][i]);
